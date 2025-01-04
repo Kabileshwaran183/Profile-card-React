e7061a4df9751bc3c4710377c5365d5bd773b0c0
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-01-04 14:42:13 +0530

    adding the links for profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 
 function App() {
-  
+  const sendEmail = () => {
+    window.location.href = "mailto:your-email@example.com?subject=Hello&body=This%20is%20a%20test%20email";
+};
+  const linkedIn = () => {
+    href="https://www.linkedin.com/in/kabileshwaranB/"
+  }
   return (
     <>
     <div className="outer-box">
@@ -14,8 +19,10 @@ function App() {
         <h6 className="tit-2">React Developer</h6>
         <p className="tit-3"><u>webiste-page</u></p>
         <div className="btn-section">
-          <button id="mail-btn"><img src="/email.png"/>Email</button>
-          <button id="lin-btn"><img src="/linkedin.png"/>LinkedIn</button> 
+          <button id="mail-btn" onClick={sendEmail}><img src="/email.png"/>Email</button>
+          <a href="https://www.linkedin.com/in/kabileshwaranB/"
+            target="_blank" ><button id="lin-btn" onClick={linkedIn}><img src="/linkedin.png"/>LinkedIn</button> 
+          </a>
         </div>
         <h4 className="about">About</h4>
         <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
@@ -25,9 +32,14 @@ function App() {
 
       <div className="footer-section">
         <img className="soc-icons" src="/Twitter Icon.png" alt="twitter" />
+        <a href="https://www.facebook.com/kabileshwaran.kabileshwaran.50" target = "_blank">
         <img className="soc-icons" src="/Facebook Icon.png" alt="facebook" />
-        <img className="soc-icons" src="/Instagram Icon.png" alt="instagram" />
-        <img className="soc-icons" src="GitHub Icon.png" alt="github" />
+        </a>
+        <a href="https://www.instagram.com/kabileshwaran183/" target="_blank">
+          <img className="soc-icons" src="/Instagram Icon.png" alt="instagram" />
+        </a>
+        <a href="https://github.com/Kabileshwaran183" target="_blank"><img className="soc-icons" src="GitHub Icon.png" alt="github" />
+        </a>
       </div>
 
     </div>
